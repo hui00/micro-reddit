6c43ec67bda097526232a8dfb8270fe3c9245ba2
hui00 <kevinbrinkschulte@hotmail.de>
2022-10-28 19:52:36 +0200

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# email: string
+# password_digest: string
+# password_confirmation: string, optional
+
+
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
