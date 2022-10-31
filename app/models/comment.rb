ee324450315a340c96d93d697996d2974a384381
hui00 <kevinbrinkschulte@hotmail.de>
2022-10-31 21:41:05 +0100

    improve validates add associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@
 # body with minimum length
 
 class Comment < ApplicationRecord
-  validates :body, presence: true, length: { minimum: 20 }
+  validates :body, presence: true, length: { minimum: 3 }
   belongs_to :user
   belongs_to :post
 end
